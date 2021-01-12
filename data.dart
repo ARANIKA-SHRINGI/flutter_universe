7b1bab8bf2df557bfca675f68a131fdc4e442994
ARANIKA-SHRINGI <76878257+ARANIKA-SHRINGI@users.noreply.github.com>
2021-01-12 12:01:28 +0530

    Add files via upload

---
+++ data.dart
@@ -0,0 +1,84 @@
+class PlanetInfo {
+  final int position;
+  final String name;
+  final String iconImage;
+  final String description;
+  final List<String> images;
+
+  PlanetInfo(
+    this.position, {
+    this.name,
+    this.iconImage,
+    this.description,
+    this.images,
+  });
+}
+
+List<PlanetInfo> planets = [
+  PlanetInfo(1,
+      name: ' MR.VIMAL DAGA',
+      iconImage: 'assets/vimal daga.png',
+      description:
+          "WORLD RECORD HOLDER| TEDxSpeker | Philanthropist | Sr.Principal Consultant|Entrereneur| Founder LW Inforatics  ",
+      images: [
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQoXpfJm8aumjUo7DYwdmLcQpWZsewCN-ONPg&usqp=CAU',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMZCMW1urOSALr95BjbHllR2YEpAxw6kaP0Q&usqp=CAU',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQo79onPgNNKvKuoiiedJ_W9RMjDrMoPCGdgA&usqp=CAU',
+        'https://pbs.twimg.com/media/ETfAne6UMAALm4Q.jpg'
+      ]),
+  PlanetInfo(2,
+      name: 'अर्थ',
+      iconImage: '',
+      description:
+          " ",
+      images: [
+         'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQoXpfJm8aumjUo7DYwdmLcQpWZsewCN-ONPg&usqp=CAU',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMZCMW1urOSALr95BjbHllR2YEpAxw6kaP0Q&usqp=CAU',
+        
+      ]),
+  PlanetInfo(3,
+      name: 'अर्थ',
+       iconImage: 'assets/logo-removebg-preview.png',
+      description:
+          " PRESENTING the LIFE Changing Program - ARTH✨       once in a life, every technical student should be a part of ARTH program",
+      images: [
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJeWDLTc1qV-2J0Bas9SDxlEjXlCS82dpqRg&usqp=CAU',
+       
+      ]),
+  PlanetInfo(4,
+      name: ' new concept',
+      iconImage: 'assets/laern.png',
+      description:"As entrepreneurs, we see new opportunities unfolding daily. New technologies emerge regularly that we need to master to succeed. These opportunities are wonderful, but at times they can be intimidating.",
+ 
+      images: [ 
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMZCMW1urOSALr95BjbHllR2YEpAxw6kaP0Q&usqp=CAU',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQo79onPgNNKvKuoiiedJ_W9RMjDrMoPCGdgA&usqp=CAU',
+        'https://pbs.twimg.com/media/ETfAne6UMAALm4Q.jpg']),
+  PlanetInfo(5,
+      name: ' Real Projects',
+      iconImage: 'assets/com.png',
+      description:
+          "Today we have access to the best brainpower through books, audio and video recordings, and live coaching. Be willing to part with your TME (Time, Money, Energy) to get what you need. Find the people who know what you need to know and be willing to pay them to help you. Hire pros for their time through PayPal, WorldPay, Bitcoin or other technologies. Use tools like Skype or Google+ Hangouts. Saturate your mind with books, magazines and quality learning.",
+      images: [
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTRFw5PnMq32wCDOUjsOZqaw9FyHb11Tj5-Gg&usqp=CAU',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRCJ7KbMN2hE_D_kjzEXhNtY1s5KnS2SknvaA&usqp=CAU',
+      ]),
+  PlanetInfo(6,
+      name: ' ',
+      iconImage: 'assets/help.png',
+      description:
+          "24/7 services often employ complex schemes that ensure their resistance to potential disruption, resilience in the event of disruption, and minimum standards of overall reliability.",
+      images: []),
+  PlanetInfo(7,
+      name: 'DSA',
+      iconImage: 'assets/dsa.png',
+      description:
+          "The DSA algorithm works in the framework of public-key cryptosystems and is based on the algebraic properties of modular exponentiation, together with the discrete logarithm problem, which is considered to be computationally intractable. The algorithm uses a key pair consisting of a public key and a private key.  ",
+      images: []),
+  PlanetInfo(8,
+      name: ' READY',
+      iconImage: 'assets/ready.png',
+      description:
+          "We are ready to train you for the better future  Need guidance on what it takes to learn & integrate cutting edge technologies?                  THE CLASSES START 12th SEPHTEMBER 2020",
+      images: []),
+];
---
+++ home_page.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_universe/detail_page.dart';
+import 'constants.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+import 'data.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: gradientEndColor,
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [gradientStartColor, gradientEndColor],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.3, 0.7])),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'ARTH -THE SCHOOL OF TECHNOLOGIES',
+                      style: TextStyle(
+                        fontFamily: 'Avenir',
+                        fontSize: 44,
+                        color: const Color(0xffffffff),
+                        fontWeight: FontWeight.w900,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  
+                  ],
+                ),
+              ),
+              Container(
+                height: 500,
+                padding: const EdgeInsets.only(left: 32),
+                child: Swiper(
+                  itemCount: planets.length,
+                  itemWidth: MediaQuery.of(context).size.width - 2 * 65,
+                  layout: SwiperLayout.STACK,
+                  pagination: SwiperPagination(
+                    builder:
+                        DotSwiperPaginationBuilder(activeSize: 15, space: 8),
+                  ),
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          PageRouteBuilder(
+                            pageBuilder: (context, a, b) => DetailPage(
+                              planetInfo: planets[index],
+                            ),
+                          ),
+                        );
+                      },
+                      child: Stack(
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              SizedBox(height: 80),
+                              Card(
+                                elevation: 8,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(32),
+                                ),
+                                color: Colors.white,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(32.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      SizedBox(height: 100),
+                                      Text(
+                                        planets[index].name,
+                                        style: TextStyle(
+                                          fontFamily: 'Avenir',
+                                          fontSize: 34,
+                                          color: const Color(0xff47455f),
+                                          fontWeight: FontWeight.w900,
+                                        ),
+                                        textAlign: TextAlign.left,
+                                      ),
+                                      Text(
+                                        'LinuxWorld Information Pvt Ltd',
+                                        style: TextStyle(
+                                          fontFamily: 'Avenir',
+                                          fontSize: 13,
+                                          color: primaryTextColor,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                        textAlign: TextAlign.left,
+                                      ),
+                                      SizedBox(height: 15),
+                                      Row(
+                                        children: <Widget>[
+                                          Text(
+                                            'Know more',
+                                            style: TextStyle(
+                                              fontFamily: 'Avenir',
+                                              fontSize: 10,
+                                              color: secondaryTextColor,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                            textAlign: TextAlign.left,
+                                          ),
+                                          Icon(
+                                            Icons.arrow_forward,
+                                            color: secondaryTextColor,
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Hero(
+                            tag: planets[index].position,
+                            child: Image.asset(planets[index].iconImage),
+                          ),
+                          Positioned(
+                            right: 10,
+                            bottom: 20,
+                            child: Text(
+                              planets[index].position.toString(),
+                              style: TextStyle(
+                                fontFamily: 'Avenir',
+                                fontSize: 100,
+                                color: primaryTextColor.withOpacity(0.08),
+                                fontWeight: FontWeight.w900,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+     
+    );
+  }
+}
